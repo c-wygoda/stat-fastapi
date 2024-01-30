a327f365d012e2bdc7809da0ebe81a6956888dd9
Christian Wygoda <christian.wygoda@wygoda.net>
2024-01-30 15:24:08 +0100

    Use pydantic's AwareDatetime for RFC3339 support

--- stat_fastapi/models/datetime.py
+++ stat_fastapi/models/datetime.py
@@ -1,38 +1,43 @@
 from datetime import datetime
-from typing import Annotated, Any, TypeAliasType
+from typing import Annotated, Any, Callable, TypeAliasType
 
-from pydantic import BeforeValidator, PlainSerializer, WithJsonSchema
-
-# TODO: This is very much happy path...
-
-
-def validate(val: Any) -> tuple[datetime, datetime]:
-    if not isinstance(val, str):
-        raise ValueError("not a valid datetime interval string")
-
-    start, end = val.split("/", 1)
-
-    start = datetime.fromisoformat(start)
-    if start.tzinfo is None:
-        raise ValueError("start datetime must have timezone info")
-
-    end = datetime.fromisoformat(end)
-    if start.tzinfo is None:
-        raise ValueError("end datetime must have timezone info")
-
-    return (start, end)
+from pydantic import (
+    AfterValidator,
+    AwareDatetime,
+    BeforeValidator,
+    WithJsonSchema,
+    WrapSerializer,
+)
 
 
-def serialize(value: tuple[datetime, datetime]) -> str:
-    return f"{value[0].isoformat()}/{value[1].isoformat()}"
+def validate_before(value: Any):
+    if isinstance(value, str):
+        start, end = value.split("/", 1)
+        return (start, end)
+    return value
+
+
+def validate_after(value: tuple[datetime, datetime]):
+    if value[1] < value[0]:
+        raise ValueError("end before start")
+    return value
+
+
+def serialize(
+    value: tuple[datetime, datetime],
+    serializer: Callable[[tuple[datetime, datetime]], tuple[str, str]],
+) -> str:
+    serialized = serializer(value)
+    return f"{serialized[0]}/{serialized[1]}"
 
 
 DatetimeInterval = TypeAliasType(
     "DatetimeInterval",
     Annotated[
-        tuple[datetime, datetime],
-        BeforeValidator(validate),
-        PlainSerializer(serialize, return_type=str),
+        tuple[AwareDatetime, AwareDatetime],
+        BeforeValidator(validate_before),
+        AfterValidator(validate_after),
+        WrapSerializer(serialize, return_type=str),
         WithJsonSchema({"type": "string"}, mode="serialization"),
     ],
 )
