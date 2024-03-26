8c3bef1d8a6c7a3fd8b2a05d72ddbeb5fb05b3cb
Christian Wygoda <christian.wygoda@wygoda.net>
2024-03-26 15:48:22 +0100

    fixup! Initial project setup

--- stat_fastapi/tests/root_test.py
+++ stat_fastapi/tests/root_test.py
@@ -1,48 +1,47 @@
+from typing import Generator
 from warnings import warn
 
 from fastapi import status
 from fastapi.testclient import TestClient
 from pytest import fixture
 
-from stat_fastapi.models.root import RootResponse
-
 from .utils import TYPE_JSON, find_link
 from .warnings import StatSpecWarning
 
 
 @fixture(scope="module")
-def data(stat_client: TestClient):
+def data(stat_client: TestClient) -> Generator[dict, None, None]:
     res = stat_client.get("/")
 
     assert res.status_code == status.HTTP_200_OK
     assert res.headers["Content-Type"] == TYPE_JSON
 
-    yield RootResponse(**res.json())
+    yield res.json()
 
 
-def test_root_self_link(data: RootResponse, url_for):
-    link = find_link(data.links, "self")
+def test_root_self_link(data, url_for):
+    link = find_link(data["links"], "self")
     if link is None:
         warn(StatSpecWarning("GET / Link[rel=self] should exist"))
     else:
-        assert link.type == TYPE_JSON
-        assert link.href == url_for("/")
+        assert link["type"] == TYPE_JSON
+        assert link["href"] == str(url_for("/"))
 
 
-def test_root_service_description_link(data: RootResponse, base_url: str):
-    link = find_link(data.links, "service-description")
+def test_root_service_description_link(data, base_url: str):
+    link = find_link(data["links"], "service-description")
     if link is None:
         warn(StatSpecWarning("GET / Link[rel=service-description] should exist"))
 
     else:
-        assert link.type == TYPE_JSON
-        assert str(link.href) == f"{base_url.rstrip('/')}/openapi.json"
+        assert link["type"] == TYPE_JSON
+        assert link["href"] == f"{base_url.rstrip('/')}/openapi.json"
 
 
-def test_root_service_docs_link(data: RootResponse, base_url: str):
-    link = find_link(data.links, "service-docs")
+def test_root_service_docs_link(data, base_url: str):
+    link = find_link(data["links"], "service-docs")
     if link is None:
         warn(StatSpecWarning("GET / Link[rel=service-docs] should exist"))
     else:
-        assert link.type == "text/html"
-        assert str(link.href) == f"{base_url.rstrip('/')}/docs"
+        assert link["type"] == "text/html"
+        assert link["href"] == f"{base_url.rstrip('/')}/docs"
